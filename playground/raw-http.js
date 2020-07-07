57c6984d0d2db2e55c17decba864fa1946f7f5aa
Faiz Hameed <faizftb@gmail.com>
2020-07-07 20:52:07 +0530

    http request core module

---
+++ playground/raw-http.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+const url =
+  "http://api.weatherstack.com/current?access_key=37a0e1a544688e07b8b42f50d6314e15&query=${78},${45}&units=m";
+
+const request = http.request(url, (response) => {
+  let data = "";
+  // we have to listen for individula chunks to come in
+  response.on("data", (chunk) => {
+    data += chunk.toString();
+  });
+
+  response.on("end", () => {
+    const body = JSON.parse(data);
+    console.log(body);
+  });
+});
+
+request.on("error", () => {
+  console.log("An Error", error);
+});
+
+request.end();
