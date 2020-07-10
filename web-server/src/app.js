c09070b6e9b8b92474bbc8d2b83024a80a25044a
Faiz Hameed <faizftb@gmail.com>
2020-07-10 21:14:38 +0530

    json file sending from the server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,27 @@
-const express =require('express')
+const express = require("express");
 
-const app= express()
+const app = express();
 
 // app.com
 
-app.get('',(req,res )=>{
-    res.send('Hello Faiz')
-})
-app.get('/about',(req,res )=>{
-    res.send('About Us')
-})
+app.get("", (req, res) => {
+  res.send("Hello Faiz");
+});
+app.get("/about", (req, res) => {
+  res.send("About Us");
+});
 
-app.get('/weather',(req,res )=>{
-    res.send('About Us')
-})
+app.get("/weather", (req, res) => {
+  res.send("About Us");
+});
 
-app.listen(3212,()=>{
-    console.log("server running on port 3212")
-})
+app.get("/help", (req, res) => {
+  res.send({
+    name: "faiz",
+    age: 27,
+  });
+});
+
+app.listen(3212, () => {
+  console.log("server running on port 3212");
+});
