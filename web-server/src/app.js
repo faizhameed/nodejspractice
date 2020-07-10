88d6f4d95a331d52263b630e3d3d2c9a1be2c9de
Faiz Hameed <faizftb@gmail.com>
2020-07-10 22:05:25 +0530

    templating with nodejs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,27 +1,30 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
 // app.com
+const staticHtml = path.join(__dirname, "../public");
+
+app.use(express.static(staticHtml));
+app.set("view engine", "hbs");
+
+app.get("", (req, res) => {
+  res.render("index");
+});
 
 app.get("", (req, res) => {
   res.send("Hello Faiz");
 });
-app.get("/about", (req, res) => {
-  res.send("About Us");
-});
 
 app.get("/weather", (req, res) => {
-  res.send("About Us");
-});
-
-app.get("/help", (req, res) => {
   res.send({
-    name: "faiz",
-    age: 27,
+    location: "Dubai",
+    longitude: 24,
+    latitude: 32,
   });
 });
 
-app.listen(3212, () => {
-  console.log("server running on port 3212");
+app.listen(3000, () => {
+  console.log("server running on port 3000");
 });
