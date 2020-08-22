3d23ccb650fffe7bf0b6eccb0ba5406647811cec
Faiz Hameed <faizftb@gmail.com>
2020-08-22 09:28:53 +0530

    promise findOneAndDelete model Method

---
+++ playground/promise-chaining-2.js
@@ -0,0 +1,13 @@
+require("../../task-manager/src/db/mongoose");
+
+const Task = require("../../task-manager/src/models/task");
+
+//5f3fc2019240fac4316dbdba
+
+Task.findByIdAndDelete("5f3fc2019240fac4316dbdba")
+  .then((task) => {
+    console.log(task);
+    return Task.countDocuments({ complete: true });
+  })
+  .then((result) => console.log(result))
+  .catch((e) => console.log(e));
